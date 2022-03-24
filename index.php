7805997b7d9f54e29562408060c1dce79b98bd72
frozar <fabien.rozar@gmail.com>
2022-03-24 15:39:44 +0400

    Try to use the production DB.

--- index.php
+++ index.php
@@ -12,28 +12,36 @@
 
 <?php
 try {
-// Souvent on identifie cet objet par la variable $conn ou $db
-    $mysqlClient = new PDO(
-        'mysql:host=localhost;dbname=sprint_06;charset=utf8',
-        'sprint_06',
-        'sprint_06'
-    );
+// dev configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
+        $mysqlClient = new PDO(
+            'mysql:host=localhost;dbname=sprint_06;charset=utf8',
+            'sprint_06',
+            'sprint_06'
+        );
+    }
+
+    // prod configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+        $mysqlClient = new PDO(
+            'mysql:host=109.234.164.161;dbname=sc1lgvu9627_rozar-fabien.sprint-06;charset=utf8',
+            $_SERVER['DB_USER'],
+            $_SERVER['DB_PASSWORD']
+        );
+    }
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
-// die('hello die' . $mysqlClient);
 
 $articleStatement = $mysqlClient->prepare('SELECT * FROM article');
 $articleStatement->execute();
 $articles = $articleStatement->fetchAll();
 
-$toto;
-
 // On affiche chaque recette une à une
 foreach ($articles as $article) {
     ?>
-    <p><?php echo $article['reference']; ?></p>
-    <?php
+     <p><?php echo $article['reference']; ?></p>
+     <?php
 }
 ?>
 
